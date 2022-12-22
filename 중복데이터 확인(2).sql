4b16fae00877f962b3ac07dd22ffdbd534f96f94
wj2321 <83794055+wonJJun2@users.noreply.github.com>
2022-12-23 01:31:54 +0900

    Create 중복데이터 확인(2).sql

---
+++ 중복데이터 확인(2).sql
@@ -0,0 +1,6 @@
+-- 그룹바이를 통한 중복확인
+SELECT  columns ,  -- 중복 데이터
+         COUNT(columns) -- 중복 갯수
+FROM Table              -- 중복조사를 할 테이블 이름
+GROUP BY columns      -- 중복되는 항목 조사를 할 컬럼
+HAVING COUNT(columns) > 1 ;
