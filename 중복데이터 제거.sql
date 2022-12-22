69021c7139b6a83440b91d48867908f1f2612674
wj2321 <83794055+wonJJun2@users.noreply.github.com>
2022-12-22 18:02:33 +0900

    Delete 중복데이터 제거.sql

--- 중복데이터 제거.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-FROM Table
-WHERE id IN 
-(
-    SELECT id FROM (SELECT id FROM Table GROUP BY column1,column2 HAVING count(*) > 1) temp_table
-)
-
-# Table : 테이블 이름
-# column1,2 : 테이블 내 겹치는 칼럼
