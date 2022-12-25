3612114ca0db31baaacbdafb51593d5ea13ccb51
wj2321 <83794055+wonJJun2@users.noreply.github.com>
2022-12-25 22:32:25 +0900

    Update 중복데이터 확인(2).sql

--- 중복데이터 확인(2).sql
+++ 중복데이터 확인(2).sql
@@ -3,4 +3,4 @@ SELECT  columns ,  -- 중복 데이터
          COUNT(columns) -- 중복 갯수
 FROM Table              -- 중복조사를 할 테이블 이름
 GROUP BY columns      -- 중복되는 항목 조사를 할 컬럼
-HAVING COUNT(columns) > 1 ;
+HAVING COUNT(columns) > 1;
