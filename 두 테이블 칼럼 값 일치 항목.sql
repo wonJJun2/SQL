cbfba7c9910153adeb04de0f3364e19a4b5a3e1d
wj2321 <83794055+wonJJun2@users.noreply.github.com>
2023-01-05 19:07:16 +0900

    Update 두 테이블 칼럼 값 일치 항목.sql

--- 두 테이블 칼럼 값 일치 항목.sql
+++ 두 테이블 칼럼 값 일치 항목.sql
@@ -2,3 +2,5 @@
 SELECT * FROM 테이블A WHERE 컬럼A in (select 컬럼B from 테이블B)
 -- 불일치
 SELECT * FROM 테이블A WHERE 컬럼A not in (select 컬럼B from 테이블B)
+-- 일치 삭제
+DELETE FROM 테이블A WHERE 컬럼A in (select 컬럼B from 테이블B)
