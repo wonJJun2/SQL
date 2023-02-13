436798baf2ad88e61ce1348fc13c5b8333810123
wj2321 <83794055+wonJJun2@users.noreply.github.com>
2023-02-13 20:42:08 +0900

    Update Basic.sql

--- Basic.sql
+++ Basic.sql
@@ -3,3 +3,5 @@ SELECT * FROM 테이블 이름 WHERE author = 'egoing' ORDER BY id DESC;
 
 INSERT INTO 테이블 이름 (columns1, columns2, columns3) 
 VALUES (columns1의 값, columns2의 값, columns3의 값);
+
+SELECT * FROM 테이블 이름 groupby columns;
