1a0edd93db9601a6ee9d473b46bcf581adb87697
wj2321 <83794055+wonJJun2@users.noreply.github.com>
2023-02-21 21:19:29 +0900

    Update Basic.sql

--- Basic.sql
+++ Basic.sql
@@ -1,5 +1,6 @@
 SELECT * FROM 테이블 이름 WHERE author = 'egoing';
 SELECT * FROM 테이블 이름 WHERE author = 'egoing' ORDER BY id DESC;
+SELECT * FROM 테이블 이름 WHERE author = 'egoing' ORDER BY id ASC;
 
 INSERT INTO 테이블 이름 (columns1, columns2, columns3) 
 VALUES (columns1의 값, columns2의 값, columns3의 값);
