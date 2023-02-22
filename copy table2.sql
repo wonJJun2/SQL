73b03f164afc061ec0aba5f1a327696b501d8de9
Won Jun YANG <83794055+wonJJun2@users.noreply.github.com>
2023-02-22 17:13:13 +0900

    Update copy table2.sql

--- copy table2.sql
+++ copy table2.sql
@@ -1,5 +1,5 @@
 -- 테이블 구조 복사
-CREATE TABLE 복사할 테이블 명 AS SELECT * FROM 기존 테이블 WHERE 1=2
+CREATE TABLE 복사할 테이블 명 AS SELECT * FROM 기존 테이블 WHERE 1=2;
 
 -- 다른 db 테이블 복사
 CREATE TABLE 복사할DB.테이블명 LIKE 원본.테이블명;
